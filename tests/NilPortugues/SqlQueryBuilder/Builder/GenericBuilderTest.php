645f2fb5cebea26729aaef87c4d8d54cb2463c47
Nil Portugués Calderó <nilopc@gmail.com>
2014-09-12 21:43:24 +0200

    Added missing tests for GenericBuilder

--- tests/NilPortugues/SqlQueryBuilder/Builder/GenericBuilderTest.php
+++ tests/NilPortugues/SqlQueryBuilder/Builder/GenericBuilderTest.php
@@ -11,6 +11,7 @@
 namespace Tests\NilPortugues\SqlQueryBuilder\Builder;
 
 use NilPortugues\SqlQueryBuilder\Builder\GenericBuilder;
+use NilPortugues\SqlQueryBuilder\Manipulation\Select;
 
 class GenericBuilderTest extends \PHPUnit_Framework_TestCase
 {
@@ -70,6 +71,33 @@ class GenericBuilderTest extends \PHPUnit_Framework_TestCase
     {
         $className = '\NilPortugues\SqlQueryBuilder\Manipulation\Intersect';
         $this->assertInstanceOf($className, $this->writer->intersect());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_minus_object()
+    {
+        $className = '\NilPortugues\SqlQueryBuilder\Manipulation\Minus';
+        $this->assertInstanceOf($className, $this->writer->minus(new Select('table1'), new Select('table2')));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_union_object()
+    {
+        $className = '\NilPortugues\SqlQueryBuilder\Manipulation\Union';
+        $this->assertInstanceOf($className, $this->writer->union());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_create_union_all_object()
+    {
+        $className = '\NilPortugues\SqlQueryBuilder\Manipulation\UnionAll';
+        $this->assertInstanceOf($className, $this->writer->unionAll());
     }
 
     /**
