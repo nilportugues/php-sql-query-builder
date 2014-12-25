8dc37ddc57aca82f93d779146583b8134a93f1f2
Nil Portugués Calderó <nilopc@gmail.com>
2014-12-25 12:28:13 +0100

    Moved out from SelectWriter the column logic to its own class

---
+++ src/Manipulation/ColumnQuery.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 12/25/14
+ * Time: 12:12 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NilPortugues\SqlQueryBuilder\Manipulation;
+
+
+/**
+ * Class ColumnQuery
+ * @package NilPortugues\SqlQueryBuilder\Manipulation
+ */
+class ColumnQuery {
+
+} 
